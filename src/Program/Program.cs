c9218dea2d56bd7c12b2ba8195584a1a59a62199
apochiega <julianpijuan15@gmail.com>
2022-12-14 09:21:18 -0300

    Fixed some more errors

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -13,20 +13,24 @@ public class Program
     
     public static void Main(string[] args)
     {
-        compuertaAnd and1 = new compuertaAnd("AND-1");
-        IInput primer_valor  = new LogicInput(1);
-        IInput segundo_valor  = new LogicInput(1);
-        and1.AddInput("A", primer_valor);
-        and1.AddInput("B", segundo_valor);
+        compuertaAnd AND = new compuertaAnd("AND-1");
+        IInput FirstValue  = new LogicInput(1);
+        IInput SecondValue  = new LogicInput(1);
+        AND.AddInput("A", FirstValue);
+        AND.AddInput("B", SecondValue);
 
-        compuertaOr or2 = new compuertaOr("OR-2");
-        IInput tercer_valor = new LogicInput(0);
-        or2.AddInput("C", tercer_valor);
-        or2.AddInput(and1.Name, and1);
 
-        compuertaNot not3 = new compuertaNot("NOT-3");
-        not3.AddInput(or2.Name, or2);
+
+        compuertaOr OR = new compuertaOr("OR-2");
+        IInput ThirdValue = new LogicInput(0);
+        OR.AddInput("C", ThirdValue);
+        OR.AddInput(AND.Name, AND);
+
+
+
+        compuertaNot NOT = new compuertaNot("NOT-3");
+        NOT.AddInput(OR.Name, OR);
         
-        Console.WriteLine(not3.Calculate());
+        Console.WriteLine(NOT.Calculate());
     }
 }
